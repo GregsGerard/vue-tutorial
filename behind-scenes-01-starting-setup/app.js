4eda0b6637ef1bbedbfa5806f9aed3dd016ebd71
Gregory Gerard <gregory.gerard@ankorstore.com>
2022-10-14 14:18:33 +0200

    feat: lesson 61

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,52 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+
+let message = 'Hello!';
+
+let longMessage = message + ' World!';
+
+console.log(longMessage);
+
+message = 'Hello!!!!';
+
+console.log(longMessage);
+
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+}
+
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+    if (key === 'message') {
+      target.longMessage = value + 'World!!!'
+    }
+    target.message = value;
+  },
+};
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+console.log(proxy);
