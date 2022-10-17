ed584f0064d76b34573acefe817b059b306fca04
Gregory Gerard <gregory.gerard@ankorstore.com>
2022-10-17 10:10:13 +0200

    feat: lesson 85

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li>blublu</li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123 45567 90',
+          email: 'manuel@localhostcom',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0987 65432 10',
+          email: 'manuel@localhostcom',
+        },
+      ]
+    };
+  },
+};
+</script>
