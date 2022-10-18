de5c1d7099edc9063da7d4d64f16c00847c9a357
Gregory Gerard <gregory.gerard@ankorstore.com>
2022-10-18 18:43:02 +0200

    feat: assignement 6

---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="addInfos">
+    <ul>
+      <li>
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="inputUserName"/>
+      </li>
+      <li>
+        <label for="age">Age</label>
+        <input type="text" id="age" v-model="inputAge"/>
+      </li>
+      <li>
+        <button>Add Username informations</button>
+      </li>
+    </ul>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-infos'],
+  data() {
+    return {
+      inputUserName: '',
+      inputAge: '',
+    };
+  },
+  methods: {
+    addInfos() {
+      if (this.inputUserName != '' && this.inputAge != '') {
+        this.$emit('add-infos', {username: this.inputUserName, age: this.inputAge});
+        this.inputUserName = this.inputAge = '';
+      }
+    },
+  },
+};
+</script>
